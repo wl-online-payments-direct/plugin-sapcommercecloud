878b511d764bb209ae3398e8364c388f2c24f6b8
Anonymous <anonymous@anonymous.com>
2021-05-04 12:46:36 +0000

    ING-597 - Create ingenico endpoints for OCC

--- hybris/bin/custom/ingenico/ingenicoogonedirectocc/src/com/ingenico/ogone/direct/occ/helpers/IngenicoHelper.java
+++ hybris/bin/custom/ingenico/ingenicoogonedirectocc/src/com/ingenico/ogone/direct/occ/helpers/IngenicoHelper.java
@@ -14,8 +14,13 @@ import org.springframework.stereotype.Component;
 import com.ingenico.direct.domain.DirectoryEntry;
 import com.ingenico.direct.domain.PaymentProduct;
 import com.ingenico.ogone.direct.facade.IngenicoCheckoutFacade;
-import com.ingenico.ogone.direct.payment.dto.DirectoryEntryListWsDTO;
+import com.ingenico.ogone.direct.facade.IngenicoUserFacade;
+import com.ingenico.ogone.direct.order.data.IngenicoPaymentInfoData;
+import com.ingenico.ogone.direct.payment.dto.DirectoryEntryWsDTO;
+import com.ingenico.ogone.direct.payment.dto.HostedTokenizationResponseWsDTO;
 import com.ingenico.ogone.direct.payment.dto.PaymentProductListWsDTO;
+import com.ingenico.ogone.direct.payment.dto.ProductDirectoryWsDTO;
+import com.ingenico.ogone.direct.payment.dto.SavedTokenWsDTO;
 
 
 @Component("ingenicoHelper")
@@ -26,19 +31,33 @@ public class IngenicoHelper {
     @Resource(name = "ingenicoCheckoutFacade")
     private IngenicoCheckoutFacade ingenicoCheckoutFacade;
 
+    @Resource(name = "ingenicoUserFacade")
+    private IngenicoUserFacade ingenicoUserFacade;
+
     private int getIdealIndex(List<PaymentProduct> availablePaymentMethods) {
-      return Iterables.indexOf(availablePaymentMethods, paymentProduct -> PAYMENT_METHOD_IDEAL.equals(paymentProduct.getId()));
+        return Iterables.indexOf(availablePaymentMethods, paymentProduct -> PAYMENT_METHOD_IDEAL.equals(paymentProduct.getId()));
     }
 
-    public void fillIdealIssuers(PaymentProductListWsDTO paymentProductListWsDTO, List<PaymentProduct> availablePaymentMethods,String fields) {
+    public void fillIdealIssuers(PaymentProductListWsDTO paymentProductListWsDTO, List<PaymentProduct> availablePaymentMethods, String fields) {
         final List<DirectoryEntry> idealIssuers = ingenicoCheckoutFacade.getIdealIssuers(availablePaymentMethods);
 
-        if(CollectionUtils.isNotEmpty(idealIssuers)) {
-            final DirectoryEntryListWsDTO directoryEntryListWsDTO = getDataMapper().map(idealIssuers, DirectoryEntryListWsDTO.class, fields);
+        if (CollectionUtils.isNotEmpty(idealIssuers)) {
+
+            final List<DirectoryEntryWsDTO> directoryEntryListWsDTO = getDataMapper().mapAsList(idealIssuers, DirectoryEntryWsDTO.class, fields);
+            ProductDirectoryWsDTO productDirectoryWsDTO = new ProductDirectoryWsDTO();
+            productDirectoryWsDTO.setEntries(directoryEntryListWsDTO);
+
             final int idealIndex = getIdealIndex(availablePaymentMethods);
-            paymentProductListWsDTO.getPaymentProducts().get(idealIndex).setDirectoryEntries(directoryEntryListWsDTO);
+            paymentProductListWsDTO.getPaymentProducts().get(idealIndex).setProductDirectory(productDirectoryWsDTO);
         }
     }
+
+    public void fillSavedTokens(HostedTokenizationResponseWsDTO hostedTokenizationResponseWsDTO, String fields) {
+        final List<IngenicoPaymentInfoData> ingenicoPaymentInfos = ingenicoUserFacade.getIngenicoPaymentInfos(true);
+        final List<SavedTokenWsDTO> savedTokenWsDTOS = getDataMapper().mapAsList(ingenicoPaymentInfos, SavedTokenWsDTO.class, fields);
+        hostedTokenizationResponseWsDTO.setTokens(savedTokenWsDTOS);
+    }
+
 
     protected DataMapper getDataMapper() {
         return dataMapper;
