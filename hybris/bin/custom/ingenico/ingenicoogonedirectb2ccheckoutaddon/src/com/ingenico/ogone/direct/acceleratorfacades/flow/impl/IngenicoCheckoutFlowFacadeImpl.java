5a3364427b7227a312339df4b8336255a186beb6
Anonymous <anonymous@bravebison.com>
2021-05-05 22:53:48 +0000

    ING-633 - Use Saved Tokens

--- hybris/bin/custom/ingenico/ingenicoogonedirectb2ccheckoutaddon/src/com/ingenico/ogone/direct/acceleratorfacades/flow/impl/IngenicoCheckoutFlowFacadeImpl.java
+++ hybris/bin/custom/ingenico/ingenicoogonedirectb2ccheckoutaddon/src/com/ingenico/ogone/direct/acceleratorfacades/flow/impl/IngenicoCheckoutFlowFacadeImpl.java
@@ -2,16 +2,21 @@ package com.ingenico.ogone.direct.acceleratorfacades.flow.impl;
 
 import de.hybris.platform.acceleratorfacades.flow.impl.DefaultCheckoutFlowFacade;
 import de.hybris.platform.commercefacades.order.data.CartData;
+import de.hybris.platform.commerceservices.order.CommercePaymentProviderStrategy;
 
 import com.ingenico.ogone.direct.acceleratorfacades.flow.IngenicoCheckoutFlowFacade;
 import com.ingenico.ogone.direct.enums.IngenicoCheckoutTypesEnum;
 
 public class IngenicoCheckoutFlowFacadeImpl extends DefaultCheckoutFlowFacade implements IngenicoCheckoutFlowFacade {
-
+    private static final String INGENICO_PROVIDER="INGENICO";
+    private CommercePaymentProviderStrategy commercePaymentProviderStrategy;
     @Override
     public boolean hasNoPaymentInfo() {
-        final CartData cartData = getCheckoutCart();
-        return cartData == null || cartData.getIngenicoPaymentInfo() == null;
+        if(INGENICO_PROVIDER.equals(commercePaymentProviderStrategy.getPaymentProvider())) {
+            final CartData cartData = getCheckoutCart();
+            return cartData == null || cartData.getIngenicoPaymentInfo() == null;
+        }
+        return super.hasNoPaymentInfo();
     }
 
 
@@ -21,5 +26,9 @@ public class IngenicoCheckoutFlowFacadeImpl extends DefaultCheckoutFlowFacade im
         return cartData == null ||
                 cartData.getIngenicoPaymentInfo() == null ||
                 !IngenicoCheckoutTypesEnum.HOSTED_TOKENIZATION.equals(cartData.getIngenicoPaymentInfo().getIngenicoCheckoutType());
+    }
+
+    public void setCommercePaymentProviderStrategy(CommercePaymentProviderStrategy commercePaymentProviderStrategy) {
+        this.commercePaymentProviderStrategy = commercePaymentProviderStrategy;
     }
 }
