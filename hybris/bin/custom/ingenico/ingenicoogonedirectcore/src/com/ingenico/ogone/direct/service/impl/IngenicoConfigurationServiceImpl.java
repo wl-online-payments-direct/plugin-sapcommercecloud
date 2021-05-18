e4260435163b8bc8f9e0aa44c628b001997fd9ea
Anonymous <anonymous@anonymous.com>
2021-05-18 17:17:20 +0100

    ING-589 - Handling webhooks and pending payment status

--- hybris/bin/custom/ingenico/ingenicoogonedirectcore/src/com/ingenico/ogone/direct/service/impl/IngenicoConfigurationServiceImpl.java
+++ hybris/bin/custom/ingenico/ingenicoogonedirectcore/src/com/ingenico/ogone/direct/service/impl/IngenicoConfigurationServiceImpl.java
@@ -7,8 +7,7 @@ import java.util.Objects;
 import de.hybris.platform.store.BaseStoreModel;
 import de.hybris.platform.store.services.BaseStoreService;
 
-import org.springframework.cache.annotation.Cacheable;
-
+import com.ingenico.ogone.direct.dao.IngenicoConfigurationDao;
 import com.ingenico.ogone.direct.exception.IngenicoConfigurationNotFoundException;
 import com.ingenico.ogone.direct.model.IngenicoConfigurationModel;
 import com.ingenico.ogone.direct.service.IngenicoConfigurationService;
@@ -16,9 +15,15 @@ import com.ingenico.ogone.direct.service.IngenicoConfigurationService;
 public class IngenicoConfigurationServiceImpl implements IngenicoConfigurationService {
 
     private BaseStoreService baseStoreService;
+    private IngenicoConfigurationDao ingenicoConfigurationDao;
 
     @Override
-    @Cacheable(value = "ingenicoConfiguration", key = "T(com.ingenico.ogone.direct.cache.IngenicoCacheKeyGenerator).generateKey(false,#baseStoreModel.uid,'config')")
+    public IngenicoConfigurationModel getIngenicoConfigurationByWebhookKey(String webhookKey) {
+        validateParameterNotNull(webhookKey, "webhookKey cannot be null");
+        return ingenicoConfigurationDao.findIngenicoConfigurationByWebhookKey(webhookKey);
+    }
+
+    @Override
     public IngenicoConfigurationModel getIngenicoConfiguration(final BaseStoreModel baseStoreModel) {
         validateParameterNotNull(baseStoreModel, "baseStore cannot be null");
         final IngenicoConfigurationModel ingenicoConfiguration = baseStoreModel.getIngenicoConfiguration();
@@ -29,13 +34,11 @@ public class IngenicoConfigurationServiceImpl implements IngenicoConfigurationSe
     }
 
     @Override
-    @Cacheable(value = "ingenicoConfiguration", key = "T(com.ingenico.ogone.direct.cache.IngenicoCacheKeyGenerator).generateKey(true,'config')")
     public IngenicoConfigurationModel getCurrentIngenicoConfiguration() {
         return getIngenicoConfiguration(baseStoreService.getCurrentBaseStore());
     }
 
     @Override
-    @Cacheable(value = "ingenicoConfiguration", key = "T(com.ingenico.ogone.direct.cache.IngenicoCacheKeyGenerator).generateKey(false,#baseStoreModel.uid,'merchant')")
     public String getMerchantId(final BaseStoreModel baseStoreModel) {
         validateParameterNotNull(baseStoreModel, "baseStore cannot be null");
         final IngenicoConfigurationModel currentIngenicoConfiguration = getIngenicoConfiguration(baseStoreModel);
@@ -43,7 +46,6 @@ public class IngenicoConfigurationServiceImpl implements IngenicoConfigurationSe
     }
 
     @Override
-    @Cacheable(value = "ingenicoConfiguration", key = "T(com.ingenico.ogone.direct.cache.IngenicoCacheKeyGenerator).generateKey(true,'merchant')")
     public String getCurrentMerchantId() {
         return getMerchantId(baseStoreService.getCurrentBaseStore());
     }
@@ -51,5 +53,9 @@ public class IngenicoConfigurationServiceImpl implements IngenicoConfigurationSe
 
     public void setBaseStoreService(BaseStoreService baseStoreService) {
         this.baseStoreService = baseStoreService;
+    }
+
+    public void setIngenicoConfigurationDao(IngenicoConfigurationDao ingenicoConfigurationDao) {
+        this.ingenicoConfigurationDao = ingenicoConfigurationDao;
     }
 }
