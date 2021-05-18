eead8bd53dd4bb78fb8383142c8b8b1b284632e4
Martina Peneva <martina.peneva@greenlightcommerce.com>
2021-05-18 16:59:49 +0300

    Update testConnection action

--- hybris/bin/custom/ingenico/ingenicoogonedirectbackoffice/backoffice/src/com/ingenico/ogone/direct/actions/IngenicoTestConnectionAction.java
+++ hybris/bin/custom/ingenico/ingenicoogonedirectbackoffice/backoffice/src/com/ingenico/ogone/direct/actions/IngenicoTestConnectionAction.java
@@ -5,22 +5,29 @@ import javax.annotation.Resource;
 import com.hybris.cockpitng.actions.ActionContext;
 import com.hybris.cockpitng.actions.ActionResult;
 import com.hybris.cockpitng.actions.CockpitAction;
-import com.ingenico.ogone.direct.service.IngenicoConfigurationService;
+import com.ingenico.ogone.direct.model.IngenicoConfigurationModel;
+import com.ingenico.ogone.direct.service.IngenicoServiceMethodsService;
+import org.zkoss.zhtml.Messagebox;
 
 public class IngenicoTestConnectionAction implements CockpitAction {
 
    private final static String TEST_CONNECTION_OK_RESULT = "OK";
 
    @Resource
-   private IngenicoConfigurationService ingenicoConfigurationService;
+   private IngenicoServiceMethodsService ingenicoServiceMethodsService;
 
    @Override
    public ActionResult<String> perform(ActionContext actionContext) {
-      String testConnectionResult = ingenicoConfigurationService.testConnection();
+      IngenicoConfigurationModel ingenicoConfigurationModel = (IngenicoConfigurationModel) actionContext.getData();
+      String testConnectionResult = ingenicoServiceMethodsService.testConnection(ingenicoConfigurationModel);
 
+      ActionResult<String> result = null;
       if (testConnectionResult.equals(TEST_CONNECTION_OK_RESULT)) {
-         return new ActionResult<String>(ActionResult.SUCCESS, TEST_CONNECTION_OK_RESULT);
+         result = new ActionResult<String>(ActionResult.SUCCESS, TEST_CONNECTION_OK_RESULT);
+      } else {
+         result = new ActionResult<String>(ActionResult.ERROR);
       }
-      return new ActionResult<String>(ActionResult.ERROR);
+      Messagebox.show(result.getData() + " (" + result.getResultCode() + ")");
+      return result;
    }
 }
