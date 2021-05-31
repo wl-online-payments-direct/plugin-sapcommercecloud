a14d63087bf5cf6c10a688d0fd20293fe4a785b9
Martina Peneva <martina.peneva@greenlightcommerce.com>
2021-05-31 15:58:16 +0300

    ING-587 Implement ingenico payment action

--- hybris/bin/custom/ingenico/ingenicoogonedirectcore/src/com/ingenico/ogone/direct/factory/IngenicoClientFactory.java
+++ hybris/bin/custom/ingenico/ingenicoogonedirectcore/src/com/ingenico/ogone/direct/factory/IngenicoClientFactory.java
@@ -1,8 +1,11 @@
 package com.ingenico.ogone.direct.factory;
 
 import com.ingenico.direct.Client;
+import com.ingenico.ogone.direct.model.IngenicoConfigurationModel;
 
 public interface IngenicoClientFactory {
 
     Client getClient();
+
+    Client getClient(IngenicoConfigurationModel ingenicoConfigurationModel);
 }
