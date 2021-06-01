c0af6a72db4ba993537cec12fb5881e382cde86a
Anonymous <anonymous@anonymous.com>
2021-06-01 23:25:48 +0100

    ING-560 - automatic capture cronjob ING-665 - review paymenTransactions

---
+++ hybris/bin/custom/ingenico/ingenicoogonedirectcore/src/com/ingenico/ogone/direct/dao/IngenicoOrderDao.java
@@ -0,0 +1,11 @@
+package com.ingenico.ogone.direct.dao;
+
+import java.util.List;
+
+import de.hybris.platform.core.model.order.OrderModel;
+import de.hybris.platform.order.daos.OrderDao;
+
+public interface IngenicoOrderDao extends OrderDao {
+
+    List<OrderModel> findIngenicoOrdersToCapture();
+}
