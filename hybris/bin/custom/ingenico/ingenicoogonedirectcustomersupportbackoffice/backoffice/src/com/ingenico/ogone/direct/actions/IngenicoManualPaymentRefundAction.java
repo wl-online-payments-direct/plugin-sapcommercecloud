a00a3bb506dfd985b43a303a33dead061eca3501
Martina Peneva <martina.peneva@greenlightcommerce.com>
2021-06-09 17:36:26 +0300

    ING-588 Create refund button

---
+++ hybris/bin/custom/ingenico/ingenicoogonedirectcustomersupportbackoffice/backoffice/src/com/ingenico/ogone/direct/actions/IngenicoManualPaymentRefundAction.java
@@ -0,0 +1,6 @@
+package com.ingenico.ogone.direct.actions;
+
+public class IngenicoManualPaymentRefundAction {
+
+//   ingenicoBusinessProcessService.triggerReturnProcessEvent(order, INGENICO_EVENT_REFUND);
+}
