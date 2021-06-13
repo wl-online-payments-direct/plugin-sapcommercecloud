f5653a11fd86935f4777f105b31f506852e9ac3c
Martina Peneva <martina.peneva@greenlightcommerce.com>
2021-06-13 18:24:39 +0300

    ING-588 update refund action

--- hybris/bin/custom/ingenico/ingenicoogonedirectcustomersupportbackoffice/backoffice/src/com/ingenico/ogone/direct/actions/IngenicoManualPaymentRefundAction.java
+++ hybris/bin/custom/ingenico/ingenicoogonedirectcustomersupportbackoffice/backoffice/src/com/ingenico/ogone/direct/actions/IngenicoManualPaymentRefundAction.java
@@ -1,8 +1,6 @@
 package com.ingenico.ogone.direct.actions;
 
-import static com.ingenico.ogone.direct.constants.IngenicoogonedirectcoreConstants.INGENICO_EVENT_REFUND;
 import static com.ingenico.ogone.direct.constants.IngenicoogonedirectcoreConstants.PAYMENT_STATUS_ENUM.REFUND_REQUESTED;
-import static de.hybris.platform.core.enums.PaymentStatus.INGENICO_CAPTURED;
 
 import javax.annotation.Resource;
 
@@ -11,17 +9,19 @@ import com.hybris.cockpitng.actions.ActionResult;
 import com.hybris.cockpitng.actions.CockpitAction;
 import com.ingenico.direct.domain.RefundResponse;
 import com.ingenico.ogone.direct.model.IngenicoConfigurationModel;
-import com.ingenico.ogone.direct.service.IngenicoBusinessProcessService;
 import com.ingenico.ogone.direct.service.IngenicoPaymentService;
 import com.ingenico.ogone.direct.service.IngenicoTransactionService;
+import de.hybris.platform.basecommerce.enums.ReturnStatus;
 import de.hybris.platform.core.model.order.OrderModel;
+import de.hybris.platform.omsbackoffice.actions.returns.ManualRefundAction;
+import de.hybris.platform.payment.model.PaymentTransactionModel;
 import de.hybris.platform.payment.enums.PaymentTransactionType;
 import de.hybris.platform.payment.model.PaymentTransactionEntryModel;
-import de.hybris.platform.payment.model.PaymentTransactionModel;
+import de.hybris.platform.returns.model.ReturnRequestModel;
 import de.hybris.platform.store.BaseStoreModel;
 import org.zkoss.zhtml.Messagebox;
 
-public class IngenicoManualPaymentRefundAction implements CockpitAction<OrderModel, OrderModel> {
+public class IngenicoManualPaymentRefundAction extends ManualRefundAction implements CockpitAction<ReturnRequestModel, ReturnRequestModel> {
 
    @Resource
    private IngenicoPaymentService ingenicoPaymentService;
@@ -29,11 +29,9 @@ public class IngenicoManualPaymentRefundAction implements CockpitAction<OrderMod
    @Resource
    private IngenicoTransactionService ingenicoTransactionService;
 
-   @Resource
-   private IngenicoBusinessProcessService ingenicoBusinessProcessService;
-
-   @Override public ActionResult<OrderModel> perform(ActionContext<OrderModel> actionContext) {
-      OrderModel order = actionContext.getData();
+   @Override public ActionResult<ReturnRequestModel> perform(ActionContext<ReturnRequestModel> actionContext) {
+      ReturnRequestModel returnRequestModel = actionContext.getData();
+      OrderModel order = returnRequestModel.getOrder();
 
       final PaymentTransactionEntryModel paymentTransactionToRefund = getPaymentTransactionToRefund(order);
       final IngenicoConfigurationModel ingenicoConfiguration = getIngenicoConfiguration(order);
@@ -41,31 +39,26 @@ public class IngenicoManualPaymentRefundAction implements CockpitAction<OrderMod
       RefundResponse refundResponse = ingenicoPaymentService.refundPayment(ingenicoConfiguration, paymentTransactionToRefund.getRequestId(), paymentTransactionToRefund.getPaymentTransaction().getPlannedAmount(), paymentTransactionToRefund.getCurrency().getIsocode());
 
       //result
-      ActionResult<OrderModel> result = null;
+      ActionResult<ReturnRequestModel> result = null;
       String resultMessage = null;
-      ingenicoTransactionService.updatePaymentTransaction(paymentTransactionToRefund.getPaymentTransaction(),
-            paymentTransactionToRefund.getRequestId(),
-            refundResponse.getStatus(),
-            refundResponse.getStatus(),
-            refundResponse.getRefundOutput().getAmountOfMoney(),
-            PaymentTransactionType.REFUND_STANDALONE);
 
       if (REFUND_REQUESTED.getValue().equals(refundResponse.getStatus())) {
-         ingenicoBusinessProcessService.triggerOrderProcessEvent(order, INGENICO_EVENT_REFUND);
-         result = new ActionResult<OrderModel>(ActionResult.SUCCESS, order);
+         ingenicoTransactionService.updatePaymentTransaction(paymentTransactionToRefund.getPaymentTransaction(),
+               paymentTransactionToRefund.getRequestId(),
+               refundResponse.getStatus(),
+               refundResponse.getStatus(),
+               refundResponse.getRefundOutput().getAmountOfMoney(),
+               PaymentTransactionType.REFUND_FOLLOW_ON);
+         setReturnRequestStatus(returnRequestModel, ReturnStatus.PAYMENT_REVERSED);
+         result = new ActionResult<ReturnRequestModel>(ActionResult.SUCCESS, returnRequestModel);
          resultMessage = actionContext.getLabel("action.manualrefund.success");
       } else {
-         result = new ActionResult<OrderModel>(ActionResult.ERROR, order);
+         result = new ActionResult<ReturnRequestModel>(ActionResult.ERROR, returnRequestModel);
          resultMessage = actionContext.getLabel("action.manualrefund.failure");
       }
       Messagebox.show(resultMessage + ", " + refundResponse.getStatus() + ", (" + result.getResultCode() + ")");
 
       return result;
-   }
-
-   @Override public boolean canPerform(ActionContext<OrderModel> ctx) {
-      OrderModel order = ctx.getData();
-      return order != null && INGENICO_CAPTURED.equals(order.getPaymentStatus());
    }
 
    private IngenicoConfigurationModel getIngenicoConfiguration(OrderModel orderModel) {
@@ -77,8 +70,18 @@ public class IngenicoManualPaymentRefundAction implements CockpitAction<OrderMod
       final PaymentTransactionModel finalPaymentTransaction = order.getPaymentTransactions().get(order.getPaymentTransactions().size() - 1);
       return finalPaymentTransaction.getEntries()
             .stream()
-            .filter(entry -> PaymentTransactionType.AUTHORIZATION.equals(entry.getType()))
+            .filter(entry -> PaymentTransactionType.CAPTURE.equals(entry.getType()))
             .findFirst().orElse(null);
+   }
+
+   protected void setReturnRequestStatus(final ReturnRequestModel returnRequest, final ReturnStatus status)
+   {
+      returnRequest.setStatus(status);
+      returnRequest.getReturnEntries().stream().forEach(entry -> {
+         entry.setStatus(status);
+         getModelService().save(entry);
+      });
+      getModelService().save(returnRequest);
    }
 
    public void setIngenicoPaymentService(IngenicoPaymentService ingenicoPaymentService) {
@@ -89,7 +92,4 @@ public class IngenicoManualPaymentRefundAction implements CockpitAction<OrderMod
       this.ingenicoTransactionService = ingenicoTransactionService;
    }
 
-   public void setIngenicoBusinessProcessService(IngenicoBusinessProcessService ingenicoBusinessProcessService) {
-      this.ingenicoBusinessProcessService = ingenicoBusinessProcessService;
-   }
 }
