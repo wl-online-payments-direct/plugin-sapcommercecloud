b0dd0f954906d5b047f92b7b26c902f65c39c4e4
khalil.medkouri@greenlightcommerce.com <Guari-123>
2022-08-30 16:55:56 +0100

    ING-916 : Refactor b2c recurring to use the same services for b2b recurring

--- hybris/bin/custom/worldline/worldlinedirectcore/src/com/worldline/direct/service/WorldlineCustomerAccountService.java
+++ hybris/bin/custom/worldline/worldlinedirectcore/src/com/worldline/direct/service/WorldlineCustomerAccountService.java
@@ -1,8 +1,5 @@
 package com.worldline.direct.service;
 
-import de.hybris.platform.commerceservices.search.pagedata.PageableData;
-import de.hybris.platform.commerceservices.search.pagedata.SearchPageData;
-import de.hybris.platform.core.model.order.OrderModel;
 import de.hybris.platform.core.model.order.payment.WorldlinePaymentInfoModel;
 import de.hybris.platform.core.model.user.CustomerModel;
 import de.hybris.platform.orderscheduling.model.CartToOrderCronJobModel;
@@ -19,7 +16,4 @@ public interface WorldlineCustomerAccountService {
 
     CartToOrderCronJobModel getCartToOrderCronJob(String jobCode);
 
-    SearchPageData<CartToOrderCronJobModel> getPagedCartToOrderCronJobsForUser(CustomerModel currentCustomer, PageableData pageableData);
-
-    SearchPageData<OrderModel> getOrdersForJob(String jobCode, PageableData pageableData);
 }
