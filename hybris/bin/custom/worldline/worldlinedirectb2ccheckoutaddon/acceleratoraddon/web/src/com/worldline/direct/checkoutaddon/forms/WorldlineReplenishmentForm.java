f6ae41d34a98bfc6b6e0b4f3ee373a137e7497c3
Martina Peneva <martinapeneva@Martinas-MacBook-Pro-2.local>
2023-09-14 13:34:08 +0300

    ING-1084 Add replenishmentForm to cartPage

---
+++ hybris/bin/custom/worldline/worldlinedirectb2ccheckoutaddon/acceleratoraddon/web/src/com/worldline/direct/checkoutaddon/forms/WorldlineReplenishmentForm.java
@@ -0,0 +1,91 @@
+package com.worldline.direct.checkoutaddon.forms;
+
+import de.hybris.platform.b2bacceleratorfacades.order.data.B2BReplenishmentRecurrenceEnum;
+import de.hybris.platform.cronjob.enums.DayOfWeek;
+
+import java.util.Date;
+import java.util.List;
+
+public class WorldlineReplenishmentForm {
+   private boolean replenishmentOrder;
+   private Date replenishmentStartDate;
+   private Date replenishmentEndDate;
+   private String nDays;
+   private String nWeeks;
+   private String nMonths;
+   private String nthDayOfMonth;
+   private B2BReplenishmentRecurrenceEnum replenishmentRecurrence;
+   private List<DayOfWeek> nDaysOfWeek;
+
+   public boolean isReplenishmentOrder() {
+      return replenishmentOrder;
+   }
+
+   public void setReplenishmentOrder(boolean replenishmentOrder) {
+      this.replenishmentOrder = replenishmentOrder;
+   }
+
+   public Date getReplenishmentStartDate() {
+      return replenishmentStartDate;
+   }
+
+   public void setReplenishmentStartDate(Date replenishmentStartDate) {
+      this.replenishmentStartDate = replenishmentStartDate;
+   }
+
+   public Date getReplenishmentEndDate() {
+      return replenishmentEndDate;
+   }
+
+   public void setReplenishmentEndDate(Date replenishmentEndDate) {
+      this.replenishmentEndDate = replenishmentEndDate;
+   }
+
+   public String getnDays() {
+      return nDays;
+   }
+
+   public void setnDays(String nDays) {
+      this.nDays = nDays;
+   }
+
+   public String getnWeeks() {
+      return nWeeks;
+   }
+
+   public void setnWeeks(String nWeeks) {
+      this.nWeeks = nWeeks;
+   }
+
+   public String getnMonths() {
+      return nMonths;
+   }
+
+   public void setnMonths(String nMonths) {
+      this.nMonths = nMonths;
+   }
+
+   public String getNthDayOfMonth() {
+      return nthDayOfMonth;
+   }
+
+   public void setNthDayOfMonth(String nthDayOfMonth) {
+      this.nthDayOfMonth = nthDayOfMonth;
+   }
+
+   public B2BReplenishmentRecurrenceEnum getReplenishmentRecurrence() {
+      return replenishmentRecurrence;
+   }
+
+   public void setReplenishmentRecurrence(B2BReplenishmentRecurrenceEnum replenishmentRecurrence) {
+      this.replenishmentRecurrence = replenishmentRecurrence;
+   }
+
+   public List<DayOfWeek> getnDaysOfWeek() {
+      return nDaysOfWeek;
+   }
+
+   public void setnDaysOfWeek(List<DayOfWeek> nDaysOfWeek) {
+      this.nDaysOfWeek = nDaysOfWeek;
+   }
+}
