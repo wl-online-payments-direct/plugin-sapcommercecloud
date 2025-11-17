0f8ea4e88cc839c35b237f20e81127a4b51446d7
James <james.johnstone@bravebison.com>
2025-11-17 17:26:35 +0000

    WL6SAP-4: Fixing error with payment product specific info.

--- hybris/bin/custom/worldline/worldlinedirectcore/src/com/worldline/direct/populator/hostedcheckout/WorldlineHostedCheckoutRedirectPopulator.java
+++ hybris/bin/custom/worldline/worldlinedirectcore/src/com/worldline/direct/populator/hostedcheckout/WorldlineHostedCheckoutRedirectPopulator.java
@@ -58,6 +58,7 @@ public class WorldlineHostedCheckoutRedirectPopulator implements Populator<Abstr
             case WorldlinedirectcoreConstants.PAYMENT_METHOD_CHEQUES_VACANCE_CONNECT:
                 RedirectPaymentProduct5403SpecificInput redirectPaymentProduct5403SpecificInput = new RedirectPaymentProduct5403SpecificInput();
                 redirectPaymentProduct5403SpecificInput.setCompleteRemainingPaymentAmount(true);
+                redirectPaymentMethodSpecificInput.setPaymentProduct5403SpecificInput(redirectPaymentProduct5403SpecificInput);
             default:
                 // No Specific parameter needed for this paymentMethod
                 break;
