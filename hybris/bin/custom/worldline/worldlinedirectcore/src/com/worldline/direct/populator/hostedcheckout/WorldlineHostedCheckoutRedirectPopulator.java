7b14a930c6dcbed3c3284d36da9a4bcbbdce15d6
James <james.johnstone@bravebison.com>
2026-02-11 14:41:20 +0000

    WP6SAP-108: Added breaks to switch statements to correct logic path where multiple specific inputs could be sent incorrectly.

--- hybris/bin/custom/worldline/worldlinedirectcore/src/com/worldline/direct/populator/hostedcheckout/WorldlineHostedCheckoutRedirectPopulator.java
+++ hybris/bin/custom/worldline/worldlinedirectcore/src/com/worldline/direct/populator/hostedcheckout/WorldlineHostedCheckoutRedirectPopulator.java
@@ -57,14 +57,17 @@ public class WorldlineHostedCheckoutRedirectPopulator implements Populator<Abstr
                 RedirectPaymentProduct5408SpecificInput redirectPaymentProduct5408SpecificInput = new RedirectPaymentProduct5408SpecificInput();
                 redirectPaymentProduct5408SpecificInput.setInstantPaymentOnly(instantPaymentOnly);
                 redirectPaymentMethodSpecificInput.setPaymentProduct5408SpecificInput(redirectPaymentProduct5408SpecificInput);
+                break;
             case WorldlinedirectcoreConstants.PAYMENT_METHOD_CHEQUES_VACANCE_CONNECT:
                 RedirectPaymentProduct5403SpecificInput redirectPaymentProduct5403SpecificInput = new RedirectPaymentProduct5403SpecificInput();
                 redirectPaymentProduct5403SpecificInput.setCompleteRemainingPaymentAmount(true);
                 redirectPaymentMethodSpecificInput.setPaymentProduct5403SpecificInput(redirectPaymentProduct5403SpecificInput);
+                break;
             case WorldlinedirectcoreConstants.PAYMENT_METHOD_MEALVOUCHER:
                 RedirectPaymentProduct5402SpecificInput redirectPaymentProduct5402SpecificInput = new RedirectPaymentProduct5402SpecificInput();
                 redirectPaymentProduct5402SpecificInput.setCompleteRemainingPaymentAmount(true);
                 redirectPaymentMethodSpecificInput.setPaymentProduct5402SpecificInput(redirectPaymentProduct5402SpecificInput);
+                break;
             default:
                 // No Specific parameter needed for this paymentMethod
                 break;
