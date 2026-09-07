58640f84e2acd67ead0693a5c102ce03c603cb8d
James Johnstone <james.johnstone@bravebison.com>
2026-09-07 12:06:41 +0100

    WP7S-xxx: Send authorization mode for mobile wallets on Hosted Checkout
    Apple Pay was falling back to whatever authorization mode is configured
    on the Worldline account, which is not necessarily the merchant's
    configured default operation code. The card populator already sends
    authorizationMode and the redirect populator the equivalent
    requiresApproval, so mobile now does the same: SALE for payment modes
    flagged sale only, otherwise the WorldlineConfiguration default
    operation code, and omitted when none is configured.

--- hybris/bin/custom/worldline/worldlinegopaycore/src/com/worldline/gopay/populator/hostedcheckout/WorldlineHostedCheckoutMobilePopulator.java
+++ hybris/bin/custom/worldline/worldlinegopaycore/src/com/worldline/gopay/populator/hostedcheckout/WorldlineHostedCheckoutMobilePopulator.java
@@ -4,6 +4,9 @@ import com.google.common.base.Preconditions;
 import com.onlinepayments.domain.CreateHostedCheckoutRequest;
 import com.onlinepayments.domain.MobilePaymentMethodHostedCheckoutSpecificInput;
 import com.worldline.gopay.constants.WorldlinegopaycoreConstants;
+import com.worldline.gopay.enums.OperationCodesEnum;
+import com.worldline.gopay.model.WorldlineConfigurationModel;
+import com.worldline.gopay.service.WorldlinePaymentModeService;
 import de.hybris.platform.converters.Populator;
 import de.hybris.platform.core.model.order.AbstractOrderModel;
 import de.hybris.platform.core.model.order.payment.WorldlinePaymentInfoModel;
@@ -12,6 +15,8 @@ import de.hybris.platform.servicelayer.dto.converter.ConversionException;
 import static de.hybris.platform.servicelayer.util.ServicesUtil.validateParameterNotNull;
 
 public class WorldlineHostedCheckoutMobilePopulator implements Populator<AbstractOrderModel, CreateHostedCheckoutRequest> {
+
+    private WorldlinePaymentModeService worldlinePaymentModeService;
 
     @Override
     public void populate(AbstractOrderModel abstractOrderModel, CreateHostedCheckoutRequest createHostedCheckoutRequest) throws ConversionException {
@@ -22,7 +27,7 @@ public class WorldlineHostedCheckoutMobilePopulator implements Populator<Abstrac
         final WorldlinePaymentInfoModel paymentInfo = (WorldlinePaymentInfoModel) abstractOrderModel.getPaymentInfo();
 
         if (WorldlinegopaycoreConstants.PAYMENT_METHOD_TYPE.MOBILE.getValue().equals(paymentInfo.getPaymentMethod())) {
-            createHostedCheckoutRequest.setMobilePaymentMethodSpecificInput(getMobilePaymentMethodSpecificInput(paymentInfo));
+            createHostedCheckoutRequest.setMobilePaymentMethodSpecificInput(getMobilePaymentMethodSpecificInput(abstractOrderModel, paymentInfo));
         }
     }
 
@@ -30,10 +35,34 @@ public class WorldlineHostedCheckoutMobilePopulator implements Populator<Abstrac
      * Passing the payment product id through means the Hosted Checkout page opens straight into the wallet the
      * shopper already picked in the storefront (Apple Pay), rather than asking them to select it a second time.
      */
-    private MobilePaymentMethodHostedCheckoutSpecificInput getMobilePaymentMethodSpecificInput(WorldlinePaymentInfoModel paymentInfo) {
+    private MobilePaymentMethodHostedCheckoutSpecificInput getMobilePaymentMethodSpecificInput(AbstractOrderModel abstractOrderModel, WorldlinePaymentInfoModel paymentInfo) {
         final MobilePaymentMethodHostedCheckoutSpecificInput mobilePaymentMethodSpecificInput = new MobilePaymentMethodHostedCheckoutSpecificInput();
         mobilePaymentMethodSpecificInput.setPaymentProductId(paymentInfo.getId());
 
+        final String authorizationMode = getAuthorizationMode(abstractOrderModel, paymentInfo);
+        if (authorizationMode != null) {
+            mobilePaymentMethodSpecificInput.setAuthorizationMode(authorizationMode);
+        }
+
         return mobilePaymentMethodSpecificInput;
+    }
+
+    /**
+     * Without this the wallet falls back to the authorization mode configured on the Worldline account, which is not
+     * necessarily the merchant's configured default operation code. Cards and redirects already send it explicitly.
+     */
+    private String getAuthorizationMode(AbstractOrderModel abstractOrderModel, WorldlinePaymentInfoModel paymentInfo) {
+        if (worldlinePaymentModeService.isSaleOnly(String.valueOf(paymentInfo.getId()))) {
+            return OperationCodesEnum.SALE.getCode();
+        }
+        final WorldlineConfigurationModel worldlineConfiguration = abstractOrderModel.getStore().getWorldlineConfiguration();
+        if (worldlineConfiguration.getDefaultOperationCode() != null) {
+            return worldlineConfiguration.getDefaultOperationCode().getCode();
+        }
+        return null;
+    }
+
+    public void setWorldlinePaymentModeService(WorldlinePaymentModeService worldlinePaymentModeService) {
+        this.worldlinePaymentModeService = worldlinePaymentModeService;
     }
 }
--- hybris/bin/custom/worldline/worldlinegopaycore/testsrc/com/worldline/gopay/populator/hostedcheckout/WorldlineHostedCheckoutMobilePopulatorTest.java
+++ hybris/bin/custom/worldline/worldlinegopaycore/testsrc/com/worldline/gopay/populator/hostedcheckout/WorldlineHostedCheckoutMobilePopulatorTest.java
@@ -2,12 +2,18 @@ package com.worldline.gopay.populator.hostedcheckout;
 
 import com.onlinepayments.domain.CreateHostedCheckoutRequest;
 import com.worldline.gopay.constants.WorldlinegopaycoreConstants;
+import com.worldline.gopay.enums.OperationCodesEnum;
+import com.worldline.gopay.model.WorldlineConfigurationModel;
+import com.worldline.gopay.service.WorldlinePaymentModeService;
 import de.hybris.bootstrap.annotations.UnitTest;
 import de.hybris.platform.core.model.order.AbstractOrderModel;
 import de.hybris.platform.core.model.order.CartModel;
 import de.hybris.platform.core.model.order.payment.WorldlinePaymentInfoModel;
+import de.hybris.platform.store.BaseStoreModel;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.lang.reflect.Proxy;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
@@ -15,21 +21,54 @@ import static org.junit.Assert.assertNull;
 @UnitTest
 public class WorldlineHostedCheckoutMobilePopulatorTest {
 
+    private final WorldlineConfigurationModel worldlineConfiguration = new WorldlineConfigurationModel();
+
+    private boolean saleOnly;
+
     private WorldlineHostedCheckoutMobilePopulator populator;
 
     @Before
     public void setUp() {
         populator = new WorldlineHostedCheckoutMobilePopulator();
+        populator.setWorldlinePaymentModeService(worldlinePaymentModeService());
+
+        saleOnly = false;
+        worldlineConfiguration.setDefaultOperationCode(OperationCodesEnum.FINAL_AUTHORIZATION);
     }
 
     @Test
     public void populateSendsApplePayPaymentProductId() {
-        CreateHostedCheckoutRequest request = populate(
-              WorldlinegopaycoreConstants.PAYMENT_METHOD_TYPE.MOBILE.getValue(),
-              WorldlinegopaycoreConstants.PAYMENT_METHOD_APPLEPAY);
+        CreateHostedCheckoutRequest request = populateApplePayRequest();
 
         assertEquals(Integer.valueOf(WorldlinegopaycoreConstants.PAYMENT_METHOD_APPLEPAY),
               request.getMobilePaymentMethodSpecificInput().getPaymentProductId());
+    }
+
+    @Test
+    public void populateSendsConfiguredDefaultOperationCodeAsAuthorizationMode() {
+        CreateHostedCheckoutRequest request = populateApplePayRequest();
+
+        assertEquals(OperationCodesEnum.FINAL_AUTHORIZATION.getCode(),
+              request.getMobilePaymentMethodSpecificInput().getAuthorizationMode());
+    }
+
+    @Test
+    public void populateForcesSaleForSaleOnlyPaymentModes() {
+        saleOnly = true;
+
+        CreateHostedCheckoutRequest request = populateApplePayRequest();
+
+        assertEquals(OperationCodesEnum.SALE.getCode(),
+              request.getMobilePaymentMethodSpecificInput().getAuthorizationMode());
+    }
+
+    @Test
+    public void populateOmitsAuthorizationModeWhenNoDefaultOperationCodeIsConfigured() {
+        worldlineConfiguration.setDefaultOperationCode(null);
+
+        CreateHostedCheckoutRequest request = populateApplePayRequest();
+
+        assertNull(request.getMobilePaymentMethodSpecificInput().getAuthorizationMode());
     }
 
     @Test
@@ -39,6 +78,11 @@ public class WorldlineHostedCheckoutMobilePopulatorTest {
               WorldlinegopaycoreConstants.PAYMENT_METHOD_PAYPAL);
 
         assertNull(request.getMobilePaymentMethodSpecificInput());
+    }
+
+    private CreateHostedCheckoutRequest populateApplePayRequest() {
+        return populate(WorldlinegopaycoreConstants.PAYMENT_METHOD_TYPE.MOBILE.getValue(),
+              WorldlinegopaycoreConstants.PAYMENT_METHOD_APPLEPAY);
     }
 
     private CreateHostedCheckoutRequest populate(String paymentMethod, int paymentProductId) {
@@ -52,8 +96,31 @@ public class WorldlineHostedCheckoutMobilePopulatorTest {
         paymentInfo.setPaymentMethod(paymentMethod);
         paymentInfo.setId(paymentProductId);
 
+        BaseStoreModel store = new BaseStoreModel();
+        store.setWorldlineConfiguration(worldlineConfiguration);
+
         CartModel order = new CartModel();
         order.setPaymentInfo(paymentInfo);
+        order.setStore(store);
         return order;
+    }
+
+    private WorldlinePaymentModeService worldlinePaymentModeService() {
+        return (WorldlinePaymentModeService) Proxy.newProxyInstance(
+              WorldlinePaymentModeService.class.getClassLoader(),
+              new Class[]{WorldlinePaymentModeService.class},
+              (proxy, method, args) -> "isSaleOnly".equals(method.getName())
+                    ? Boolean.valueOf(saleOnly)
+                    : defaultValue(method.getReturnType()));
+    }
+
+    private Object defaultValue(Class<?> returnType) {
+        if (Boolean.TYPE.equals(returnType)) {
+            return Boolean.FALSE;
+        }
+        if (Integer.TYPE.equals(returnType)) {
+            return 0;
+        }
+        return null;
     }
 }
